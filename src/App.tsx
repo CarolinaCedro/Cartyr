7f1a7b6a66500d2f8c5fc9f7b119f0a10fb966c2
Carolina Cedro <75391803+CarolinaCedro@users.noreply.github.com>
2022-05-25 11:01:55 -0300

    feat: first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { GlobalStyles } from "./styles/global";
 
-function App() {
+export function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyles/>
+      <h1>Hello world!!</h1>
     </div>
   );
 }
 
-export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+:root{
+  --background: #f2f8f5
+  --blue:#0D99FF
+  --red:#F44336
+  --text-title:#363f5f
+  --text-body:#969cb3
+}
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+html{
+  @media  (max-width:1080px) {
+    font-size: 93.75%;
+  }
+
+  @media  (max-width:780px) {
+    font-size: 87.5%;
+  }
+}
+
+body{
+  background-color: var(--background);
+  -webkit-font-smoothing:antialiased;
+}
+
+button{
+  cursor: pointer;
+}
+
+[disable]{
+  opacity: 0.6;
+  cursor: not-allowed;
+}
+
+`
