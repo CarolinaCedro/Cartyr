09d010d703969784d2ee1f0812ac2d3bcb26dec0
Carolina Cedro <75391803+CarolinaCedro@users.noreply.github.com>
2022-05-28 16:52:40 -0300

    feat: create function for values of forms

--- src/components/NewTransictionModal/index.tsx
+++ src/components/NewTransictionModal/index.tsx
@@ -1,6 +1,10 @@
 import Modal from 'react-modal'
+import { FormEvent, useState } from 'react';
+import entradas from '../../assets/entradas.svg'
+import saidas from '../../assets/saidas.svg'
 import {AiOutlineClose} from 'react-icons/ai'
-import { Container } from './style';
+import { Container, TransictionTypeContainer ,ButtonRadio} from './style';
+
 
 interface NewTransictionModalProps{
   isOpen:boolean;
@@ -8,8 +12,28 @@ interface NewTransictionModalProps{
 }
 
 export function NewTransictionModal({isOpen,onRequesClose}:NewTransictionModalProps){
+
+  const [title,setTitle] = useState('');
+  const [value,setValue] = useState(0);
+  const [typeTransiction,setTypeTransiction] = useState('deposit');
+  const [category,setCategory] =  useState('')
+
+
+  function handleCreateNewTransiction(e:FormEvent){
+     e.preventDefault(); 
+
+     console.log(
+       title,
+       value,
+       typeTransiction,
+       category
+     )
+  }
+  
+
   return(
  
+    
     <Modal
        isOpen={isOpen}
        onRequestClose={onRequesClose}
@@ -19,11 +43,50 @@ export function NewTransictionModal({isOpen,onRequesClose}:NewTransictionModalPr
          <button type='button' className='react-modal-button-close' onClick={onRequesClose}>
           <AiOutlineClose fontSize={20} color={'#969cb3'}/>
          </button>
-        <Container>
+        <Container onSubmit={handleCreateNewTransiction}>
             <h2>Cadastrar transação</h2>
-            <input  placeholder='Titulo' />
-            <input  type='number' placeholder='Valor' />
-            <input  placeholder='Categoria' />
+            <input  placeholder='Titulo' value={title}
+             onChange={(e)=> setTitle(e.target.value)} 
+            />
+
+            <input  type='number' placeholder='Valor'
+            value={value}
+            onChange={(e)=> setValue(Number(e.target.value))}
+            />
+            <TransictionTypeContainer>
+              <ButtonRadio 
+               type = 'button'
+               onClick={()=> 
+               {setTypeTransiction('deposit');
+               }}
+               isActive={typeTransiction === 'deposit'}
+               activeColor='green'
+               >
+                <img src={entradas} alt="" />
+              <span>
+                  Entrada
+              </span>
+              </ButtonRadio>
+
+              <ButtonRadio  
+              type='button'
+              onClick={()=>{
+              setTypeTransiction('withdraw');
+              }}
+              isActive={typeTransiction === 'withdraw'}
+              activeColor='red'
+              >
+                <img src={saidas} alt="" />
+              <span>
+                  Saidas
+              </span>
+              </ButtonRadio>
+            </TransictionTypeContainer>
+
+            <input  placeholder='Categoria'
+            value={category}
+            onChange={(e)=> setCategory(e.target.value)}
+            />
             <button type="submit">Cadastrar</button>
         </Container>
     </Modal>
--- src/components/NewTransictionModal/style.ts
+++ src/components/NewTransictionModal/style.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {darken,transparentize} from 'polished'
 
 export const Container = styled.form`
 
@@ -44,4 +45,53 @@ button[type="submit"]{
     filter: brightness(0.9);
   }
 }
+`
+
+export const TransictionTypeContainer = styled.div`
+margin: 1rem 0;
+display: grid;
+grid-template-columns: 1fr 1fr;
+gap: 0.5rem;
+`
+
+interface ButtonRadioProps {
+  isActive:(boolean)
+  activeColor: 'green' | 'red'
+}
+
+const colors = {
+  green: '#33CC95',
+  red: '#F44336'
+}
+export const ButtonRadio = styled.button<ButtonRadioProps>`
+  height: 3rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
+
+
+  background: ${(props) => props.isActive ?
+    transparentize(0.7,colors[props.activeColor])
+   :
+   'transparent'};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover{
+    border-color: ${darken(0.1,'#d7d7d7')};
+  }
+
+  img{
+    width: 20px;
+    height: 20px;
+  }
+
+  
+  span{
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
+  }
 `
